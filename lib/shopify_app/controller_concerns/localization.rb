f764d531c0025c947e3c86766e5570e19f478cef
Ugurcan Kaya <uurcan@pasilobus.com>
2023-09-30 16:32:59 -0700

    Refactor set_locale method for proper around_action usage

--- lib/shopify_app/controller_concerns/localization.rb
+++ lib/shopify_app/controller_concerns/localization.rb
@@ -10,7 +10,7 @@ module ShopifyApp
 
     private
 
-    def set_locale(&action)
+    def set_locale
       locale = params[:locale] || I18n.default_locale
 
       # Fallback to the 2 letter language code if the requested locale unavailable
@@ -19,9 +19,14 @@ module ShopifyApp
       end
 
       session[:locale] = locale
-      I18n.with_locale(session[:locale], &action)
+
+      I18n.with_locale(session[:locale]) do
+        yield # Executes the actual controller action
+      end
     rescue I18n::InvalidLocale
-      I18n.with_locale(I18n.default_locale, &action)
+      I18n.with_locale(I18n.default_locale) do
+        yield # Executes the actual controller action with default locale
+      end
     end
   end
 end
