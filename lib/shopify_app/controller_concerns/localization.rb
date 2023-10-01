8201b5d4a7f3218c0dbedc2b81f520a9ad3e6ca1
Ugurcan Kaya <uurcan@pasilobus.com>
2023-09-30 17:16:30 -0700

    Update localization.rb

--- lib/shopify_app/controller_concerns/localization.rb
+++ lib/shopify_app/controller_concerns/localization.rb
@@ -3,30 +3,6 @@
 module ShopifyApp
   module Localization
     extend ActiveSupport::Concern
-
-    included do
-      around_action :set_locale
-    end
-
-    private
-
-    def set_locale
-      locale = params[:locale] || I18n.default_locale
-
-      # Fallback to the 2 letter language code if the requested locale unavailable
-      unless I18n.available_locales.include?(locale.to_sym)
-        locale = locale.split("-").first
-      end
-
-      session[:locale] = locale
-
-      I18n.with_locale(session[:locale]) do
-        yield # Executes the actual controller action
-      end
-    rescue I18n::InvalidLocale
-      I18n.with_locale(I18n.default_locale) do
-        yield # Executes the actual controller action with default locale
-      end
-    end
+    
   end
 end
