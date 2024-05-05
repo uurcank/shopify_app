a39e9a0c877cb5750259574a90b08b414d5f8034
Ugurcan Kaya <uurcan@pasilobus.com>
2024-05-05 02:43:02 -0700

    Update frame_ancestors.rb

--- lib/shopify_app/controller_concerns/frame_ancestors.rb
+++ lib/shopify_app/controller_concerns/frame_ancestors.rb
@@ -7,7 +7,7 @@ module ShopifyApp
     included do
       content_security_policy do |policy|
         policy.frame_ancestors(-> do
-          domain_host = current_shopify_domain || "*.#{::ShopifyApp.configuration.myshopify_domain}"
+          domain_host = current_shopify_domain || params[:shop] || "*.#{::ShopifyApp.configuration.myshopify_domain}"
           "#{ShopifyAPI::Context.host_scheme}://#{domain_host} https://admin.shopify.com"
         end)
       end
